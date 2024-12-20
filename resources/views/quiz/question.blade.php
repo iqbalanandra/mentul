ad5e606da5c8df812240c38643b14721c1a4b550
nVITTOadrianto <nvittoa@live.com>
2024-12-20 18:11:23 +0700

    tambah total pertanyaan, dan merubah logo menjadi warna putih

--- resources/views/quiz/question.blade.php
+++ resources/views/quiz/question.blade.php
@@ -2,7 +2,7 @@
     <div class="min-h-screen flex items-center justify-center bg-gradient-to-b from-indigo-900 to-gray-900">
         <div class="bg-white p-8 rounded-lg shadow-lg max-w-lg w-full">
             <h2 class="text-3xl font-bold text-indigo-800 mb-6 text-center">
-                Pertanyaan {{ $index + 1 }}
+                Pertanyaan {{ $index + 1 }} / 28
             </h2>
             <p class="text-gray-700 text-lg text-center mb-8 leading-relaxed">
                 Apakah Anda mengalami gejala berikut? <br> <strong>{{ $gejala->deskripsi }}</strong>
